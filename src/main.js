b0d69a4e060b63db1a3808f6a9edad0eaa9408c3
Kenny Halim and Theary Im <theary@>
2019-03-21 11:35:44 -0700

    add UI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,47 @@
-// import './styles.css';
-// import { Warrior } from './rpg.js'
-//
-//
-// $(document).ready(function() {
-//
-//
-// });
+import './styles.css';
+import  Warrior  from './warrior'
+
+
+$(document).ready(function() {
+  var newWarrior;
+  $("#pickCharacter").submit(function(event){
+    event.preventDefault();
+    var characterType = $("#character").val();
+    if(characterType === "warrior"){
+      newWarrior = new Warrior();
+      $("#intro").hide();
+      $("#game").show();
+      $("#charactertype").text(characterType);
+      $("#health").text("Current Health: " + newWarrior.health);
+      $("#level").text("Current Level: " + newWarrior.level);
+      $("#strength").text("Current Strength: " + newWarrior.strength);
+      $("#items").text("Current Items: " + newWarrior.items);
+    }
+    
+  });
+  $("#fight").click(function(){
+    newWarrior.fight();
+    if(newWarrior.gameOver === false){
+      $("#health").text("Current Health: " + newWarrior.health);
+      $("#level").text("Current Level: " + newWarrior.level);
+      $("#strength").text("Current Strength: " + newWarrior.strength);
+    } else {
+      $("#gameOver").show();
+      $("#game").hide();
+    }
+  });
+  
+  $("#menu").click(function(){
+    location.reload();
+  });
+  
+  $("#revive").click(function(){
+    newWarrior.revive();
+    $("#health").text("Current Health: " + newWarrior.health);
+    $("#level").text("Current Level: " + newWarrior.level);
+    $("#strength").text("Current Strength: " + newWarrior.strength);
+    $("#game").show();
+    $("#gameOver").hide();
+  });
+
+});
---
+++ src/shop.js
@@ -0,0 +1,13 @@
+export default class Shop {
+  constructor() {
+    this.item = new Map();
+    this.addItemToShop();
+  }
+  
+  addItemToShop(){
+    this.item.set("body armor", 200);
+    this.item.set("head armor", 100);
+    this.item.set("sword", 400);
+    this.item.set("healthpotion", 50);
+  }
+}
