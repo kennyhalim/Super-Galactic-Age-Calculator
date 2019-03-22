675d7b66ccf2f056121709adeedb31821eb352a8
Kenny Halim <kennyhalim98@gmail.com>
2019-03-22 14:20:38 -0700

    add code to main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,28 @@
 import './styles.css';
-// import  Calculator  from './calculator';
+import  Calculator  from './calculator';
 
 
 $(document).ready(function() {
 
   $("#ageForm").submit(function(event){
     event.preventDefault();
+    var birthday = $("#birthDate").val();
+    var newCalc = new Calculator(birthday);
+    $("#intro").hide();
+    $("#result").show();
+    $("#age").text("Your current age is: " + newCalc.getAge());
+    $("#mercuryAge").text("Your current age in Mercury is: " + newCalc.getAgeInMercury());
+    $("#venusAge").text("Your current age in Venus is: " + newCalc.getAgeInVenus());
+    $("#marsAge").text("Your current age in Mars is: " + newCalc.getAgeInMars());
+    $("#jupiterAge").text("Your current age in Jupiter is: " + newCalc.getAgeInJupiter());
+    if(newCalc.getLifeExpectancy() === -1){
+      $("#lifeExpectancy").text("You have exceeded your average life expentancy, Congratulations and stay fit and healthy!");
+    } else {
+      $("#lifeExpectancy").text("Your life expectancy is " + newCalc.getLifeExpectancy());
+    }
 
+    $("#reload").click(function(){
+      location.reload();
+    })
   });
 });
